e83f95f5dc37c51edb3044caa22d475aa3f63e08
Christopher <Chris.97e@gmail.com>
2019-05-23 22:46:23 -0500

    Express working

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// hello world git 
+var express = require('express'),
+engines = require('consolidate'),
+handlebars = require('handlebars');
+const assert = require('assert');
+var app = express();
+
+
+app.engine('hbs', engines.handlebars);
+app.set('views', './views');
+app.set('view engine', 'hbs');
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', function (request, response) {
+    response.render('home');
+});
+
+app.listen(3000, function () {
+    console.log('Aplicación ejemplo, escuchando el puerto 3000!');
+});
