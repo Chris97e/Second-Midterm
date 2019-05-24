a4b4ae67f50b0488a16586c0c3a15161caf5115b
Christopher <Chris.97e@gmail.com>
2019-05-24 02:04:32 -0500

    other pages

--- index.js
+++ index.js
@@ -27,6 +27,48 @@ app.get('/', function (request, response) {
 
 });
 
+app.get('/fun', function (request, response) {
+   
+    var contexto ={
+        
+        numero: 100,
+        visitas: 0
+
+    }
+    
+    response.render('fun', contexto);
+
+});
+
+app.get('/admi', function (request, response) {
+   
+    var contexto ={
+        
+        numero: 100,
+        visitas: 0
+
+    }
+    
+    response.render('admi', contexto);
+
+});
+
+app.get('/bore', function (request, response) {
+   
+    var contexto ={
+        
+        numero: 100,
+        visitas: 0
+
+    }
+    
+    response.render('bore', contexto);
+
+});
+
+
+
+
 app.listen(3000, function () {
     console.log('Estoy funcionando bien sensualón, ¡escuchando en el puerto 3000!');
 });
--- public/js/date.js
+++ public/js/date.js
@@ -1,1 +1,53 @@
+function paginaCargada() {
+    
+    var horas = document.querySelector('.hora');
+    var fecha = document.querySelector('.fecha');
+    var numero = document.querySelector('.numeroVisitado');
+    
+    //fecha
+    var hoy = new Date();
+    var dd = hoy.getDate();
+    var mm = hoy.getMonth() + 1;
+    var yyyy = hoy.getFullYear();
+    dd = addZero(dd);
+    mm = addZero(mm);
 
+    
+    
+    
+    fecha.innerHTML = dd + '/' + mm + '/' + yyyy;
+    setTimeout(mueveReloj,1000);
+    
+    //añadir un cero
+    function addZero(i) {
+        if (i < 10) {
+            i = '0' + i;
+        }
+        return i;
+    }
+    
+    function mostrarhora(){ 
+        var f=new Date();
+        cad=f.getHours()+":"+f.getMinutes()+":"+f.getSeconds(); 
+        window.status =cad;
+        setTimeout("mostrarhora()",1000); 
+    }
+
+    //reloj
+    function mueveReloj(){ 
+        momentoActual = new Date() 
+        hora = momentoActual.getHours() 
+        minuto = momentoActual.getMinutes() 
+        segundo = momentoActual.getSeconds() 
+
+        horaImprimible = hora + ":" + minuto + ":" + segundo 
+
+        horas.innerHTML = horaImprimible;
+        setTimeout(mueveReloj,1000)
+    }
+
+    
+    
+    
+}
+window.addEventListener('load', paginaCargada);
